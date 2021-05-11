8b528e5c8a55d4e73518c151c341be01756b8e42
CJcool06 <cjcool06@yahoo.com.au>
2021-05-11 13:16:00 +0930

    Navbar and base pages

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,16 @@ router.get('/', function(req, res, next) {
   res.sendFile('homepage.html', { root: __dirname + '/../public/' } );
 });
 
+/* GET information page. */
+router.get('/information', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.sendFile('information.html', { root: __dirname + '/../public/' } );
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.sendFile('about.html', { root: __dirname + '/../public/' } );
+});
+
 module.exports = router;
