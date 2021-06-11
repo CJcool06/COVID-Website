f46c1b11489202e4a823c3f43a013d3da5a09664
CJcool06 <30566151+CJcool06@users.noreply.github.com>
2021-06-11 14:48:26 +0930

    VUE login functionality

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,8 +1,67 @@
+/* Startup */
+loadUser();
 
-// eslint-disable-next-line no-undef
-var vueNav = new Vue({
+
+
+
+var nav = new Vue({
     el: "#nav",
     data: {
-        logged_in: false
+        loggedIn: false
     }
 });
+
+function login() {
+    // For testing
+    let email = "person.smith@fakemail.com";
+    let password = "12345";
+
+    var http = new XMLHttpRequest();
+    http.open("POST", "/login");
+    http.responseType = "json";
+    http.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+    http.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status != 200) {
+                console.log(this.response.error);
+            }
+            else {
+                userProfile.id = this.response.id;
+                userProfile.fName = this.response.fName;
+                userProfile.lName = this.response.lName;
+                userProfile.email = this.response.email;
+                userProfile.password = this.response.password
+                userProfile.accountType = this.response.accountType;
+                nav.loggedIn = true;
+            }
+        }
+    }
+
+    http.send(JSON.stringify({'email': email, 'password': password}));
+}
+
+function loadUser() {
+    var http = new XMLHttpRequest();
+    http.responseType = "json";
+
+    http.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status != 200) {
+                console.log(this.response.error);
+            }
+            else {
+                userProfile.id = this.response.id;
+                userProfile.fName = this.response.fName;
+                userProfile.lName = this.response.lName;
+                userProfile.email = this.response.email;
+                userProfile.password = this.response.password
+                userProfile.accountType = this.response.accountType;
+                nav.loggedIn = true;
+            }
+        }
+    }
+
+    http.open("GET", "/loggedin", true);
+    http.send();
+}
