546b718c5b40b198d98f36fac642b2ee4b7da9f4
CJcool06 <30566151+CJcool06@users.noreply.github.com>
2021-06-14 18:11:25 +0930

    Removed old code

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -7,7 +7,9 @@ const locations = [
 	{ lat: -34.984, lng: 138.603 }
 ];
 
-// eslint-disable-next-line no-undef
+/**
+ * Main map vue object.
+ */
 let vmap = new Vue({
 	el: "#vmap",
 	data: {
@@ -52,52 +54,6 @@ let vmap = new Vue({
 				imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
 			});
 		}
-		// addLoc: function(loc) {
-		// 	this.locations.push(loc);
-		// 	// eslint-disable-next-line no-undef
-		// 	let marker = new google.maps.Marker({
-		// 		position: loc,
-		// 		label: this.locations.length.toString(),
-		// 		title: "Covid Location"
-
-		// 	});
-
-		// 	// eslint-disable-next-line no-undef
-		// 	let infoWindow = new google.maps.InfoWindow();
-
-		// 	marker.addListener("click", () => {
-		// 		infoWindow.close();
-		// 		infoWindow.setContent(marker.getTitle() + ": " + marker.getLabel());
-		// 		infoWindow.open(marker.getMap(), marker);
-		// 	});
-
-		// 	this.markers.push(marker);
-		// 	this.update();
-		// },
-		// addRandomLoc: function() {
-		// 	let lat = boundedRandDouble(-34.920, -34.940);
-		// 	let lng = boundedRandDouble(138.590, 138.610);
-		// 	let loc = { lat: lat, lng: lng };
-		// 	this.addLoc(loc);
-		// },
-		// // Causes bug when markers are removed while clustered
-		// removeLastLoc: function() {
-		// 	let index = this.locations.length - 1;
-		// 	let marker = this.markers[index];
-		// 	marker.setMap(null);
-		// 	marker = null;
-		// 	this.locations.splice(index, 1);
-		// 	this.markers.splice(index, 1);
-		// 	this.update();
-		// }
-		// Causes bug when markers are removed while clustered
-		// removeRandomLoc: function() {
-		// 	let num = boundedRandInt(0, this.locations.length - 1);
-		// 	let marker = this.markers[num];
-		// 	marker.setMap(null);
-		// 	this.locations.splice(num, 1);
-		// 	this.markers.splice(num, 1);
-		// }
 	}
 });
 
@@ -135,11 +91,3 @@ function populateHotspotsForMap() {
 	http.open("GET", "/hotspots/map");
 	http.send();
 }
-
-// function boundedRandDouble(min, max) { // min and max included 
-// 	return Math.random() * (max - min) + min;
-// }
-
-// function boundedRandInt(min, max) {
-// 	return Math.floor(Math.random() * (max - min + 1) + min);
-// }
